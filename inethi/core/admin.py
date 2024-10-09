0e7dbcc4cf084f7c0519b82984aea36d29a39654
Keegan White <keeganthomaswhite@gmail.com>
2024-10-09 16:43:02 +0200

    fixed workflow env creation

--- inethi/core/admin.py
+++ inethi/core/admin.py
@@ -1,8 +1,11 @@
 # core/admin.py
 
 from django.contrib import admin
-from .models import User, Wallet
+from .models import User, Wallet, SmartContract, FaucetSmartContract, AccountsIndexContract
 
 # Register your models here
 admin.site.register(User)
 admin.site.register(Wallet)
+admin.site.register(SmartContract)
+admin.site.register(FaucetSmartContract)
+admin.site.register(AccountsIndexContract)
